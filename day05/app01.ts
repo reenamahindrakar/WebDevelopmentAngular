4cb768bf0ba45cc68cdaf6dbfc292858b01a7298
reenamahindrakar <reenapm@gmail.com>
2023-12-27 19:48:47 +0530

    typescripts: variables 01

---
+++ day05/app01.ts
@@ -0,0 +1,17 @@
+let num1 : number = 10
+let num2 : number = 20.51
+
+console.log('Number1 = ' + num1)
+console.log("Number2 = " + num2 )
+
+let name1 : string = 'xyz'
+console.log("Name is " + name1 )
+
+let bool1 =true
+console.log("Boolean value is " + bool1 )
+
+let obj1 = {
+    ag : 30,
+    name2 : 'abc'
+}
+console.log(obj1 )
