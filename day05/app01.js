314c8bcd36863635733d778677c9f636444e8485
reenamahindrakar <reenapm@gmail.com>
2023-12-28 21:35:13 +0530

    day05: variables declarations

---
+++ day05/app01.js
@@ -0,0 +1,13 @@
+var num1 = 10;
+var num2 = 20.51;
+console.log('Number1 = ' + num1);
+console.log("Number2 = " + num2);
+var name1 = 'xyz';
+console.log("Name is " + name1);
+var bool1 = true;
+console.log("Boolean value is " + bool1);
+var obj1 = {
+    ag: 30,
+    name2: 'abc'
+};
+console.log(obj1);
