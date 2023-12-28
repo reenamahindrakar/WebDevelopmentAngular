08807d630601ab2c6b8fa3eb0bff3305367a335b
reenamahindrakar <reenapm@gmail.com>
2023-12-28 21:38:10 +0530

    day06:variables type-object declartion

---
+++ day06/app01.js
@@ -0,0 +1,5 @@
+// javascript : Obj variable
+var obj = { a: 12, b: 'abc' };
+console.log(obj);
+console.log('Value of a:' + obj.a);
+console.log('Value of b:' + obj.b);
---
+++ day06/app01.ts
@@ -0,0 +1,8 @@
+// javascript : Obj variable
+
+let obj : {[key : string] : any} = {a : 12 , b : 'abc'}
+
+console.log(obj)
+
+console.log('Value of a:'  + obj.a)
+console.log('Value of b:'  + obj.b)
