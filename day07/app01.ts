962c4fc23984bd276928b1814b424d9dc4dd36dc
reenamahindrakar <reenapm@gmail.com>
2023-12-29 20:21:12 +0530

    day07: functions and anonomyous funtion

---
+++ day07/app01.ts
@@ -0,0 +1,15 @@
+// functions
+function sayHello () {  //function definition
+
+    console.log('Hello')
+}
+sayHello() // function call
+var fn = sayHello // refering to sayHello, not call
+ fn() // function call
+
+ //anonomyous function
+ var fn1 = function() {  
+    console.log(`This is anonomyous function`)
+}
+
+fn1()
