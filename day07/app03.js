3b735ab8f892823dfc3447b0b112233b1da1b837
reenamahindrakar <reenapm@gmail.com>
2023-12-29 20:23:00 +0530

    day07: function with parameters and return type.

---
+++ day07/app03.js
@@ -0,0 +1,5 @@
+function move(a, b) {
+    return "a = ".concat(a, " and b is ").concat(b);
+}
+var res = move(12, 'left');
+console.log(res);
---
+++ day07/app03.ts
@@ -0,0 +1,6 @@
+function move( a: number, b: string ): string {
+    return `a = ${a} and b is ${b}`
+}
+
+var res = move(12, 'left')
+console.log(res)
